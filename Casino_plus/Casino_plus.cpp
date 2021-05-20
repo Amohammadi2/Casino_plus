3ae60c526b29493fc8de5701f98ea1c3ba857aa9
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2021-05-20 22:35:11 +0430

    initial commit

---
+++ Casino_plus/Casino_plus.cpp
@@ -0,0 +1,12 @@
+﻿// Casino_plus.cpp : Defines the entry point for the application.
+//
+
+#include "Casino_plus.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
