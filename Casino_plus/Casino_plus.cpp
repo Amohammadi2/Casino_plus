bdda12a832f37e7130902388576f4200787b7f1c
ashkan mohammadi <mohammadiashkan1384@gmail.com>
2021-05-21 19:53:58 +0430

    implement and test the algorithm

--- Casino_plus/Casino_plus.cpp
+++ Casino_plus/Casino_plus.cpp
@@ -3,10 +3,40 @@
 
 #include "Casino_plus.h"
 
-using namespace std;
+template <class Item_Type>
+void CasinoRandomGenerator<Item_Type>::AddItem
+(const Item_Type& new_item, int probabilty_chance) {
+	m_rand_items.push_back({ new_item, probabilty_chance });
+	m_total_probabilities += probabilty_chance;
+}
 
-int main()
-{
-	cout << "Hello CMake." << endl;
+template <class Item_Type>
+Item_Type CasinoRandomGenerator<Item_Type>::GetRandomItem() {
+	std::uniform_int_distribution<int> distribution(1, m_total_probabilities);
+	int rand_num = distribution(m_engine);
+	for (const auto& item : m_rand_items) {
+		rand_num -= item.second;
+		if (rand_num <= 0) {
+			return item.first;
+		}
+	}
+}
+
+template <class Item_Type>
+CasinoRandomGenerator<Item_Type>::CasinoRandomGenerator()
+ : m_engine((unsigned int) time(0)) {}
+
+// test
+int main() {
+	CasinoRandomGenerator<std::string> name_rand;
+
+	name_rand.AddItem("Ashkan Mohammadi", 5);
+	name_rand.AddItem("Ilia Mohammadi", 2);
+	name_rand.AddItem("Arshia Mohammadi", 3);
+
+	for (int i = 0; i < 10; i++) {
+		std::cout << name_rand.GetRandomItem() << std::endl;
+	}
+
 	return 0;
 }
--- Casino_plus/Casino_plus.h
+++ Casino_plus/Casino_plus.h
@@ -4,5 +4,24 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
+#include <utility>
+#include <random>
+#include <ctime>
+#include <string>
+
+template <class Item_Type>
+class CasinoRandomGenerator {
+private:
+	std::vector<std::pair<Item_Type, int>> m_rand_items;
+	int m_total_probabilities = 0;
+	std::default_random_engine m_engine;
+
+public:
+	CasinoRandomGenerator();
+
+	void AddItem(const Item_Type& new_item, int probabilty_chance);
+	Item_Type GetRandomItem();
+};
 
 // TODO: Reference additional headers your program requires here.
